5c2cc52dc2dde300d692bba90bcba5719962779f
kphed <kpcrypto69@gmail.com>
2023-11-10 09:49:34 -0500

    Add Comet interface

--- src/interfaces/IComet.sol
+++ src/interfaces/IComet.sol
@@ -2,5 +2,15 @@
 pragma solidity ^0.8.0;
 
 interface IComet {
+    struct UserBasic {
+        int104 principal;
+        uint64 baseTrackingIndex;
+        uint64 baseTrackingAccrued;
+        uint16 assetsIn;
+        uint8 _reserved;
+    }
+
     function supply(address asset, uint amount) external;
+
+    function userBasic(address user) external view returns (UserBasic memory);
 }
