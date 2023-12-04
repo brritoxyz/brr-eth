4dde2d32900f1c6994314c9dc9d2f5d2deaca8d7
kphed <kpcrypto69@gmail.com>
2023-12-04 09:31:32 -0500

    Override maxShares to mitigate risk from Comet alias

--- src/BrrETH.sol
+++ src/BrrETH.sol
@@ -55,12 +55,22 @@ contract BrrETH is Ownable, ERC4626 {
 
     /**
      * @notice Returns the maximum amount of assets that can be deposited.
-     * @dev    The balance check prevents `msg.sender` from using `type(uint256).max`
-     *         as their deposit amount, which is Comet's alias for "entire balance".
+     * @dev    Prevents `msg.sender` from using `type(uint256).max` for `assets`,
+     *         which is Comet's alias for "entire balance".
      * @return uint256  Maximum amount of assets that can be deposited.
      */
     function maxDeposit(address) public view override returns (uint256) {
         return _COMET.balanceOf(msg.sender);
+    }
+
+    /**
+     * @notice Returns the maximum amount of shares that can be minted.
+     * @dev    Prevents `msg.sender` from taking advantage of Comet's usage of
+     *         `type(uint256).max` as their alias for "entire balance".
+     * @return uint256  Maximum amount of shares that can be minted.
+     */
+    function maxMint(address) public view override returns (uint256) {
+        return convertToShares(_COMET.balanceOf(msg.sender));
     }
 
     function _deposit(
