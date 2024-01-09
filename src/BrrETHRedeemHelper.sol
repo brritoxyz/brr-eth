694d7c6277669ffdbc947e21ff651a138a56d883
kphed <kpcrypto69@gmail.com>
2024-01-09 10:09:36 -0500

    Change constant var visibility to private for consistency

--- src/BrrETHRedeemHelper.sol
+++ src/BrrETHRedeemHelper.sol
@@ -9,11 +9,11 @@ import {IWETH} from "src/interfaces/IWETH.sol";
 contract BrrETHRedeemHelper {
     using SafeTransferLib for address;
 
-    IBrrETH public constant BRR_ETH =
+    IBrrETH private constant _BRR_ETH =
         IBrrETH(0xf1288441F094d0D73bcA4E57dDd07829B34de681);
-    IComet public constant COMET =
+    IComet private constant _COMET =
         IComet(0x46e6b214b524310239732D51387075E0e70970bf);
-    IWETH public constant WETH =
+    IWETH private constant _WETH =
         IWETH(0x4200000000000000000000000000000000000006);
 
     receive() external payable {}
@@ -25,12 +25,12 @@ contract BrrETHRedeemHelper {
      */
     function redeem(uint256 shares, address to) external {
         // Requires approval from the caller to spend their brrETH balance.
-        BRR_ETH.redeem(shares, address(this), msg.sender);
+        _BRR_ETH.redeem(shares, address(this), msg.sender);
 
         // Comet's alias for an "entire balance" is `type(uint256).max`.
-        COMET.withdraw(address(WETH), type(uint256).max);
+        _COMET.withdraw(address(_WETH), type(uint256).max);
 
-        WETH.withdraw(WETH.balanceOf(address(this)));
+        _WETH.withdraw(_WETH.balanceOf(address(this)));
         to.safeTransferETH(address(this).balance);
     }
 }
